d9de3820ec2f96a0ea5d65e48631e6e7a8a60296
Hwang Yoon Jo <mymelody112266@gmail.com>
2021-10-02 18:11:07 +0900

    Initial User Interface

---
+++ TTS2/TTS2/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TTS2
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.metroButton1.Click += new System.EventHandler(this.metroButton3_Click);
+        }
+
+       
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World!");
+        }
+    }
+}
